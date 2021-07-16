f1633c4ba7fa437dddb73634c601417d0bd4daf5
Ayu <AJ.RocKeteer11@gmail.com>
2021-07-16 14:32:44 +0800

    added all the necessary files for tutorial

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+import os
+
+# app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
+# app = Flask(static_folder='C:\Users\AyuwandiraRadin\Documents\projects\my_ind-webinar\demo\DTE_Lab1\DTE_Lab1\static')
+
+# On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
+# When running this app on the local machine, default the port to 8000
+port = int(os.getenv('PORT', 8000))
+
+@app.route('/')
+def index():
+    return render_template('ABCBankPage.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=port, debug=True)
